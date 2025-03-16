023ebf266cda874827899e09092297a993cb00d4
henry1310 <ng.quoc1310@gmail.com>
2025-03-16 14:22:53 +0700

    Add files via upload

---
+++ Todo4.py
@@ -0,0 +1,22 @@
+import streamlit as st
+st.title('Bé tập làm toán')
+col1,col2,col3, = st.columns(3)
+with col1:
+    a = st.number_input('a')
+with col2:
+    op= st.radio('Phép toán',('\+','\-','x',':'),horizontal=True)
+with col3:
+    b = st.number_input('b')
+
+res = st.number_input('Nhập kết quả')
+if st.button('Kiểm tra'):
+    if op == '\\+': result = a+b
+    elif op == '\\-': result = a - b
+    elif op == 'x': result = a*b
+    elif op == ':': result = a/b
+    if result == res:
+        st.success('Chính xác')
+        st.balloons()
+    else:
+        st.error(f'Sai, đáp án đúng là {result}')
+        st.snow()
